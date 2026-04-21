7a903001790f225d9a65065c0b22a75844cf8184
Gory-git <gregorio@duck.com>
2026-04-21 23:12:04 +0200

    revshell emulatore

--- firmadyne/scripts/run.mipseb.interactive.sh
+++ firmadyne/scripts/run.mipseb.interactive.sh
@@ -21,4 +21,4 @@ WORK_DIR=`get_scratch ${IID}`
 IMAGE=`get_fs ${IID}`
 KERNEL=`get_kernel "mipseb"`
 
-qemu-system-mips -m 256 -M malta -kernel ${KERNEL} -drive if=ide,format=raw,file=${IMAGE} -append "firmadyne.syscall=1 root=/dev/sda1 console=ttyS0 nandsim.parts=64,64,64,64,64,64,64,64,64,64 rdinit=/firmadyne/preInit.shrw debug ignore_loglevel print-fatal-signals=1" -serial stdio -monitor none -display none -nographic -net none
+qemu-system-mips -m 256 -M malta -kernel ${KERNEL} -drive if=ide,format=raw,file=${IMAGE} -append "firmadyne.syscall=1 root=/dev/sda1 console=ttyS0 nandsim.parts=64,64,64,64,64,64,64,64,64,64 rdinit=/firmadyne/preInit.shrw debug ignore_loglevel print-fatal-signals=1" -serial stdio -monitor none -display none -nographic -net nic,model=rtl8139 -net tap,ifname=qemu-tap0,script=no,downscript=no
